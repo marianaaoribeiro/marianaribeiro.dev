639819141fe6a6d678cd0f6e7545ceb4ff0b84db
Mariana Ribeiro <mariana.alvesdor@gmail.com>
2023-08-27 11:37:41 -0300

    Finished header

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -12,14 +12,67 @@
         />
       </div>
       <v-spacer />
-      <div class="mr-8">
-        <v-tabs background-color="primary">
-          <v-tab class="text-none">Início</v-tab>
-          <v-tab class="text-none">Sobre</v-tab>
-          <v-tab class="text-none">Tecnologias</v-tab>
-          <v-tab class="text-none">Contato</v-tab>
+      <v-app-bar-nav-icon
+        v-if="$vuetify.breakpoint.mobile"
+        @click="drawer = true"
+      ></v-app-bar-nav-icon>
+      <div class="mr-8" v-else>
+        <v-tabs v-model="tab" background-color="primary">
+          <v-tab
+            v-for="(item, index) in routes"
+            :key="index"
+            class="text-none"
+            >{{ item.name }}</v-tab
+          >
         </v-tabs>
       </div>
     </v-app-bar>
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      dark
+      app
+      temporary
+      class="primary"
+      right
+    >
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item v-for="(item, index) in routes" :key="index">
+            <v-list-item-title>{{ item.name }}</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
   </div>
 </template>
+<script>
+export default {
+  data: () => ({
+    drawer: false,
+    tab: null,
+    group: null,
+    routes: [
+      {
+        name: "Início",
+        to: "",
+      },
+      {
+        name: "Sobre",
+        to: "",
+      },
+      {
+        name: "Tecnologias",
+        to: "",
+      },
+      {
+        name: "Contato",
+        to: "",
+      },
+    ],
+  }),
+};
+</script>
